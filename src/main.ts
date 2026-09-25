7cc34c0dd26aba62baa9a586f1ceacd95dd4f360
giangdoit123 <giangdoit123@gmail.com>
2026-09-25 09:50:14 +0000

    fix: update Swagger documentation endpoint from 'api' to 'docs'

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   const port = process.env.PORT ?? 4000;
   await app.listen(port);
